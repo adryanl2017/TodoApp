75fa7e88f6af637cd85aa23a1b7b4692c06f7072
adryanl2017 <aalal2@asu.edu>
2022-05-07 23:09:14 -0700

    created button & container for list

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('ahem')
+const button = document.getElementById('todo-button')
+const container = document.getElementById('todo-list-container')
+
+button.addEventListener('click', showItems)
+
+function showItems() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+
+            console.log(json)
+            const item = document.createElement('li')
+            item.textContent = json.title
+
+            container.append(item)
+
+        })
+}
