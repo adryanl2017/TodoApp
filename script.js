6d925986d8abcf1028806dea2ec3ea155c487534
adryanl2017 <aalal2@asu.edu>
2022-05-08 00:10:56 -0700

    no longer hoisted, removed cons logs

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const button = document.getElementById('todo-button')
-const container = document.getElementById('todo-list-container')
-
-button.addEventListener('click', showItemsAsync)
-
 async function showItemsAsync() {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos')
     const json = await response.json()
-    console.log(json)
+
     for (var i = 0; i < json.length; i++) {
         const item = document.createElement('li')
         item.textContent = item.textContent + json[i].title
         container.append(item)
     }
 }
+
+const button = document.getElementById('todo-button')
+const container = document.getElementById('todo-list-container')
+
+button.addEventListener('click', showItemsAsync)
+
 
